7339ab5e0911fadc7a833cc7ee404ff19c0c8913
pelico <50446355+pelico@users.noreply.github.com>
2025-06-16 16:44:54 +0800

    Update config.js

--- config.js
+++ config.js
@@ -10,6 +10,7 @@ window.Config = {
     'm784490063-7b5da437e7f1e0d67613714d',
     'm784497419-de55aa09902ccb3ab22d548a',
     'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur2985779-cf4fd4918b65d301b3fed1bd',
   ],
 
   // 日志天数
